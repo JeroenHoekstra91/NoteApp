2f4448ea2ff9ea0e03e9e6ef6e7d297d8f443ee7
Jeroen Hoekstra <jeroenhoekstra_1991@hotmail.com>
2014-03-28 16:05:17 +0100

    Listing of notes added

--- src/nl/defacto/notitieapp/DropboxHelper.java
+++ src/nl/defacto/notitieapp/DropboxHelper.java
@@ -1,21 +1,50 @@
 package nl.defacto.notitieapp;
 
+import java.util.List;
+
 import android.app.Activity;
 import android.content.Context;
 
 import com.dropbox.sync.android.DbxAccountManager;
+import com.dropbox.sync.android.DbxException;
+import com.dropbox.sync.android.DbxException.Unauthorized;
+import com.dropbox.sync.android.DbxFileInfo;
 import com.dropbox.sync.android.DbxFileSystem;
+import com.dropbox.sync.android.DbxPath;
+import com.dropbox.sync.android.DbxPath.InvalidPathException;
 
 public class DropboxHelper {
 	static final int LINK_DB = 1;
 	
 	private DbxAccountManager mDbxAcctMgr;
 	private DbxFileSystem dbxFs;
+	private Context context;
+	private Activity activity;
 	
-	public DropboxHelper(Context context, Activity activity) {
+	public DropboxHelper(Context context, Activity activity) throws Unauthorized {
+		this.context = context;
+		this.activity = activity;
+		
+		link();
+	}
+	
+	private void link() throws Unauthorized {
 		mDbxAcctMgr = DbxAccountManager.getInstance(context, "2cavkxlkgqtngx1", "sp1cy7i81pudjaw");
 		
 		if(!mDbxAcctMgr.hasLinkedAccount())
 			mDbxAcctMgr.startLink(activity, LINK_DB);
+	}
+	
+	public List<DbxFileInfo> fetchNotes() throws InvalidPathException, DbxException {
+		dbxFs = DbxFileSystem.forAccount(mDbxAcctMgr.getLinkedAccount());
+		return dbxFs.listFolder(new DbxPath(""));
+	}
+	
+	public void saveNote() {
+		
+	}
+	
+	public void loadNote() {
+		
 	}
 }
--- src/nl/defacto/notitieapp/NoteListActivity.java
+++ src/nl/defacto/notitieapp/NoteListActivity.java
@@ -1,10 +1,17 @@
 package nl.defacto.notitieapp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+
+import com.dropbox.sync.android.DbxException.Unauthorized;
+import com.dropbox.sync.android.DbxFileInfo;
 
 public class NoteListActivity extends ListActivity {
 	static final int ACTIVITY_CREATE = 0;	
@@ -14,7 +21,14 @@ public class NoteListActivity extends ListActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_list);
-		mDbHelper = new DropboxHelper(getApplicationContext(), this);
+		
+		try {
+			mDbHelper = new DropboxHelper(getApplicationContext(), this);
+		} catch (Unauthorized e) {
+			e.printStackTrace();
+		}
+		
+		updateList();
 	}
 	
 	@Override
@@ -51,6 +65,16 @@ public class NoteListActivity extends ListActivity {
 	}
 	
 	private void updateList() {
-		// TODO: fetch file list from Dropbox.
+		try {
+			List<String> notes = new ArrayList<String>();
+			for(DbxFileInfo file : mDbHelper.fetchNotes()) {
+				notes.add(file.path.getName());
+			}
+			
+			ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.note_row, notes);
+			setListAdapter(adapter);
+		} catch (Exception e) {
+			// TODO: show exception.
+		}
 	}
 }
